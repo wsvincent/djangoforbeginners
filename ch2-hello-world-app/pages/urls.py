a0edb1cb7d1372363ae49292156d88087051f35b
William Vincent <will@wsvincent.com>
2017-12-07 08:48:59 -0500

    update to Django 2.0

--- ch2-hello-world-app/pages/urls.py
+++ ch2-hello-world-app/pages/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.homePageView, name='home'),
+    path('', views.homePageView, name='home')
 ]
