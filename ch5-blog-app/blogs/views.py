4799efe36897e6e6ea968c71475da2b0dc6ed476
William Vincent <will@wsvincent.com>
2017-12-07 11:05:38 -0500

    update ch6 to Django 2.0

--- ch5-blog-app/blogs/views.py
+++ ch5-blog-app/blogs/views.py
@@ -5,7 +5,7 @@ from . models import Post
 
 class BlogListView(ListView):
     model = Post
-    template_name = 'post_list.html'
+    template_name = 'home.html'
 
 
 class BlogDetailView(DetailView):
--- ch6-blog-app-with-forms/blogs/models.py
+++ ch6-blog-app-with-forms/blogs/models.py
@@ -1,14 +1,17 @@
-from django.urls import reverse
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=200)
     text = models.TextField()
 
     def get_absolute_url(self):
-        return reverse('post_list')
+        return reverse('home')
 
     def __str__(self):
         return self.title
