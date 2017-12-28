f2d50eef6b870fe06017b2c5de63397eb0625c8f
William Vincent <will@wsvincent.com>
2017-12-28 13:51:52 -0500

    switch to pipenv for virtual environments

--- ch4-message-board-app/posts/tests.py
+++ ch4-message-board-app/posts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for our Test Case
+        Post.objects.create(text='just a test')
+
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEquals(expected_object_name, 'just a test')
+
+class HomePageViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'home.html')
