71565a7a5e5d8691514a9359c33475456932ac55
William Vincent <will@wsvincent.com>
2018-01-26 08:45:00 -0500

    update LOGIN_REDIRECT and LOGOUT_REDIRECT to use "home" template vs /

--- ch7-blog-app-with-users/blog_project/settings.py
+++ ch7-blog-app-with-users/blog_project/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
