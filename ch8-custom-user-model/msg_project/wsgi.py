4c29a88d4f35941b1dc44f00103da0e165c280c9
William Vincent <will@wsvincent.com>
2018-01-30 14:11:42 -0500

    ch8 code on custom user model

--- ch8-custom-user-model/msg_project/wsgi.py
+++ ch8-custom-user-model/msg_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for socialauth project.
+WSGI config for msg_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialauth.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msg_project.settings")
 
 application = get_wsgi_application()
---
+++ ch8-custom-user-model/users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+admin.site.register(CustomUser, CustomUserAdmin)
