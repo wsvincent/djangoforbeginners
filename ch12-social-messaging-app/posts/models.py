82ca7ef33bc4146c33069cf73224ee9c6f2671a8
William Vincent <will@wsvincent.com>
2018-02-08 11:48:40 -0500

    add ch12 code

---
+++ ch12-social-messaging-app/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    message = models.CharField(max_length=280)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        'users.CustomUser',
+        on_delete=models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.message
---
+++ ch12-social-messaging-app/posts/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from . import models
+
+
+class PostListView(ListView):
+    model = models.Post
+    template_name = 'post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = models.Post
+    template_name = 'post_new.html'
+    fields = ['message', 'author']
+
+
+class PostDetailView(DetailView):
+    model = models.Post
+    template_name = 'post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = models.Post
+    fields = ['message']
+    template_name = 'post_edit.html'
+
+
+class PostDeleteView(DeleteView):
+    model = models.Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
