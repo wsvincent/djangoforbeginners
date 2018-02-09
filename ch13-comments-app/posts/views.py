94dd027b24ce0f48df35a6dbe2e00d3ddfe4f8e2
William Vincent <will@wsvincent.com>
2018-02-09 16:53:33 -0500

    add ch13

---
+++ ch13-comments-app/posts/views.py
@@ -0,0 +1,37 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from . import models
+
+
+class PostListView(ListView):
+    model = models.Post
+    template_name = 'post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = models.Post
+    template_name = 'post_new.html'
+    fields = ['message']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    model = models.Post
+    template_name = 'post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = models.Post
+    fields = ['message']
+    template_name = 'post_edit.html'
+
+
+class PostDeleteView(DeleteView):
+    model = models.Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
