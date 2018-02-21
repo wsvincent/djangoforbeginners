d5eb03a7c5210e3b7d8a096a83cc14e5a8d6382b
wsvincent <will@wsvincent.com>
2018-02-21 15:39:21 -0500

    pages app

--- ch3-pages-app/pages/tests.py
+++ ch3-pages-app/pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 
 
-class SimpleTests(SimpleTestCase):
+class PagesTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
--- ch3-pages-app/pages_project/wsgi.py
+++ ch3-pages-app/pages_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simple_project project.
+WSGI config for pages_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pages_project.settings")
 
 application = get_wsgi_application()
