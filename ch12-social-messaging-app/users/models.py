a71bd62b51e54b426efcee7fe553537d045d8dd0
wsvincent <will@wsvincent.com>
2018-02-22 10:56:31 -0500

    final code for ebook version

--- ch12-social-messaging-app/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser, UserManager
-
-
-class CustomUserManager(UserManager):
-    pass
-
-
-class CustomUser(AbstractUser):
-    objects = CustomUserManager()
--- ch14-authorization/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-]
