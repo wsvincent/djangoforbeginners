62b0f46948edeb5a36165912fd986a0d1e13c85e
wsvincent <will@wsvincent.com>
2018-03-22 15:24:41 -0400

    add ch9

---
+++ ch9-user-authentication/newspaper_project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+]
