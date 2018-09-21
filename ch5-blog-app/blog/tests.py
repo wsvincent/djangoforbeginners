3ded7982cc9c0bf4c87380f38a9ae16b3ea624cd
wsvincent <will@wsvincent.com>
2018-09-21 09:54:20 -0400

    remove unused Import Client in blog tests

--- ch5-blog-app/blog/tests.py
+++ ch5-blog-app/blog/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.urls import reverse
 
 from .models import Post
