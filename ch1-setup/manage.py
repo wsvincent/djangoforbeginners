f9560ffb6e5256eb4db7d0c1142104e0566fabb5
William Vincent <will@wsvincent.com>
2020-06-16 12:27:39 -0400

    rename to "config" directory

--- ch1-setup/manage.py
+++ ch1-setup/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
