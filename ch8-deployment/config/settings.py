342a9efe0a81ccc58b9960dd75b9de4c65e59cd4
Will Vincent <will@wsvincent.com>
2020-06-29 14:28:30 -0400

    update static_root

--- ch8-deployment/config/settings.py
+++ ch8-deployment/config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')] # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new!
