2b5030e1de7fd48f76b60c16fc41d33215100c37
Will Vincent <will@wsvincent.com>
2020-06-30 13:29:32 -0400

    updated Pipfiles

--- ch8-deployment/config/settings.py
+++ ch8-deployment/config/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')] # new!
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new!
 
 LOGIN_REDIRECT_URL = 'home'
