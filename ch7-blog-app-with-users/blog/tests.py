aae2879d30d398201d59ef9610f2024209171c44
Will Vincent <will@wsvincent.com>
2021-11-04 15:24:12 -0400

    switch to setUpTestData

--- ch7-blog-app-with-users/blog/tests.py
+++ ch7-blog-app-with-users/blog/tests.py
@@ -6,20 +6,17 @@ from .models import Post
 
 
 class BlogTests(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
             username="testuser", email="test@email.com", password="secret"
         )
 
-        self.post = Post.objects.create(
+        cls.post = Post.objects.create(
             title="A good title",
             body="Nice body content",
-            author=self.user,
+            author=cls.user,
         )
-
-    def test_string_representation(self):
-        post = Post(title="A sample title")
-        self.assertEqual(str(post), post.title)
 
     def test_get_absolute_url(self):  # new
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
