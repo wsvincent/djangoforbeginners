cac906a67c62cda4fea04c127265954141a2c130
Will Vincent <will@wsvincent.com>
2021-11-15 14:41:32 -0500

    revert ch4 tests

--- ch4-message-board-app/posts/tests.py
+++ ch4-message-board-app/posts/tests.py
@@ -3,22 +3,26 @@ from django.urls import reverse  # new
 from .models import Post
 
 
-class PostsTests(TestCase):
+class PostTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.post = Post.objects.create(text="This is a test!")
 
-    def test_post_content(self):
+    def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
 
-    def test_homepage_status_code(self):  # new
+    def test_url_exists_at_correct_location(self):  # new
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_homepage_uses_correct_template(self):  # new
+    def test_url_available_by_name(self):  # new
+            response = self.client.get(reverse("home"))
+            self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "home.html")
 
-    def test_homepage_content(self):  # new
+    def test_template_content(self):  # new
         response = self.client.get(reverse("home"))
         self.assertContains(response, "This is a test!")
